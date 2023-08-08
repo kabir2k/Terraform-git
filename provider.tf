928957921cbc63104a69af303cfb474887949a8a
syed ahmad kabir <124237233+kabir2k@users.noreply.github.com>
2023-08-08 11:47:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
     region = "ap-south-1"
-    access_key =  "AKIAQXMELG5ORJWTR6UW"
-    secret_key =  "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"
+    access_key =  "AKIAQXMELG5ORQK3ATN"
+    secret_key =  "Z+yd4TscTsqGi7GZvKCdmsQ2+f58IJmjj3TcFhS4"
 }
